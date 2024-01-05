c496adbcc333bc25fe4e1d63c4994837c2c12caf
isabelafsd <isabelafonsecasd@gmail.com>
2024-01-05 19:36:48 -0300

    feat: criando a estrutura base do projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExampleComponentComponent } from './features/example-component/example-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExampleComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
