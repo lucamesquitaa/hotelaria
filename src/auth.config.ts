bd1b6c244b3d532ff9b3429e3048440899afa8a4
lucamesquitaa@gmail.com <lucamesquitaa@gmail.com>
2026-03-03 22:42:43 -0300

    melhorias

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,11 +1,11 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  issuer: 'https://accounts.google.com', // endpoint OIDC do Google
+  issuer: 'https://accounts.google.com',
   strictDiscoveryDocumentValidation: false,
   clientId: '838656343224-8cr24hdeobtu00kevkhj27sudbuq8g97.apps.googleusercontent.com',
-  redirectUri: 'https://turify.com.br',
+  redirectUri: window.location.origin + '/login',
   scope: 'openid profile email',
-  responseType: 'code', // usa PKCE
+  responseType: 'code',
   showDebugInformation: true
 };
