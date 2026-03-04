8131a9bd47c23870e614b32e90afa7b1b94f53c0
lucamesquitaa@gmail.com <lucamesquitaa@gmail.com>
2026-03-03 22:16:50 -0300

    https

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -4,7 +4,7 @@ export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com', // endpoint OIDC do Google
   strictDiscoveryDocumentValidation: false,
   clientId: '167620261725-hpe1ram422hksd64t7fp5to7gbthf4cr.apps.googleusercontent.com',
-  redirectUri: 'http://149.57.203.34:8080',
+  redirectUri: 'https://turify.com.br/',
   scope: 'openid profile email',
   responseType: 'code', // usa PKCE
   showDebugInformation: true,
