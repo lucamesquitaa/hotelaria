d1e7661f43d0bf651b6afb04d13956722662aae9
lucamesquitaa@gmail.com <lucamesquitaa@gmail.com>
2026-03-05 20:20:30 -0300

    sof

--- src/app/shared/services/disponibilidade.service.ts
+++ src/app/shared/services/disponibilidade.service.ts
@@ -11,7 +11,7 @@ import { HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DisponibilidadeService extends ServiceGeneric<ResponseApi<any>> {
-  override urlServiceREST: string = "http://149.57.203.34:8080/api/MotorDeReservas/";
+  override urlServiceREST: string = "https://api.turify.com.br/api/MotorDeReservas/";
 
   constructor(public override injector: Injector) {
     super(injector);
