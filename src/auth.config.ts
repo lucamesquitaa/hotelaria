d16b05b7f150f66c8ebe0edec2cb8bfead5b4fc9
lucamesquitaa@gmail.com <lucamesquitaa@gmail.com>
2026-03-05 21:04:14 -0300

    pl

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,16 +1,17 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
+import { environment } from './environments/environment';
 
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
-  redirectUri: window.location.origin + '/dashboard',
-  postLogoutRedirectUri: window.location.origin + '/login',
+  redirectUri: environment.oauth.redirectUri,
+  postLogoutRedirectUri: environment.oauth.postLogoutRedirectUri,
   clientId: '838656343224-8cr24hdeobtu00kevkhj27sudbuq8g97.apps.googleusercontent.com',
   scope: 'openid profile email',
   responseType: 'code',
   oidc: true,
   requestAccessToken: true,
-  showDebugInformation: true,
+  showDebugInformation: !environment.production,
   strictDiscoveryDocumentValidation: false,
   useSilentRefresh: true,
-  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html'
+  silentRefreshRedirectUri: environment.oauth.silentRefreshRedirectUri
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  oauth: {
+    redirectUri: 'https://turify.com.br/dashboard',
+    postLogoutRedirectUri: 'https://turify.com.br/login',
+    silentRefreshRedirectUri: 'https://turify.com.br/silent-refresh.html'
+  }
+};
